3c3ee18abe426011d4057b43f0e67c207500f894
Sunim <sunim.54@gmail.com>
2017-06-27 10:51:40 +0545

    Showing message from index & defining message condition in compt1

--- Component1.js
+++ Component1.js
@@ -2,20 +2,10 @@ import React , {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
 
 export default class Component1 extends Component{
-  constructor(){
-    super();
-    this.state = {
-      name: 'Arya',
-      showName: true
-    }
-  }
-  
   render(){
-      let name = this.state.showName ? this.state.name : 'No Name';
     return(
       <View>
         <Text>{this.props.message}</Text>
-        <Text>{name}</Text>
       </View>
     );
   }
--- index.android.js
+++ index.android.js
@@ -1,12 +1,13 @@
 import React , {Component} from 'react';
 import {AppRegistry, Text, View} from 'react-native';
+//import Component1 from './Component1';
 import Component1 from './app/components/Component1/Component1';
 
 export default class myapp2 extends Component{
   render(){
     return(
       <View>
-        <Component1 />
+        <Component1 message="Lolita" />
       </View>
     );
   }
